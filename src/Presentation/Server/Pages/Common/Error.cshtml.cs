7e19bed954afc8cd9c69a1bbb451e2d8d18e0812
= <taufan.augusta@protonmail.com>
2022-01-09 13:40:02 +0700

    refactor: blazor file structure

--- src/Presentation/Server/Pages/Common/Error.cshtml.cs
+++ src/Presentation/Server/Pages/Common/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Server.Pages;
+namespace Server.Pages.Common;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
