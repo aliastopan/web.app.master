93dfe2e1e3960967c44a03fed7051830e6d34b99
= <taufan.augusta@protonmail.com>
2022-01-09 17:22:07 +0700

    feat: basic authentication

--- src/Application/Services/Authentication.cs
+++ src/Application/Services/Authentication.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Authorization;
 
@@ -28,8 +29,47 @@ namespace Application.Services
             var user = new ClaimsPrincipal(identity);
             var authetication = new AuthenticationState(user);
 
+            NotifyAuthenticationStateChanged(Task.FromResult(authetication));
             return await Task.FromResult(authetication);
         }
+
+        public void Authenticate(string username)
+        {
+            var claims = new Claim[]{
+                new Claim(ClaimTypes.Name, username)
+            };
+            var authType = "apiauth_type";
+            var identity = new ClaimsIdentity(claims, authType);
+            var user = new ClaimsPrincipal(identity);
+            var authetication = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(authetication));
+        }
+
+        public AuthenticationState AuthState(string username)
+        {
+            var claims = new Claim[]{
+                new Claim(ClaimTypes.Name, username)
+            };
+            var authType = "apiauth_type";
+            var identity = new ClaimsIdentity(claims, authType);
+            var user = new ClaimsPrincipal(identity);
+            var authetication = new AuthenticationState(user);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(authetication));
+            return authetication;
+        }
+
+        public ClaimsIdentity CreateIdentity(string username)
+        {
+            var claims = new Claim[]{
+                new Claim(ClaimTypes.Name, username)
+            };
+            var authType = "apiauth_type";
+            return new ClaimsIdentity(claims, authType);
+        }
+
+
 
     }
 }
--- src/Presentation/Server/Services/Middleware.cs
+++ src/Presentation/Server/Services/Middleware.cs
@@ -1,4 +1,7 @@
+using System.Text.Json;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Infrastructure.Data;
 using Application.Services;
 using Domain.Models;
 
@@ -6,16 +9,50 @@ namespace Server.Services
 {
     public class Middleware
     {
+        private readonly Authentication authentication;
         private readonly ProtectedLocalStorage localStorage;
+        private readonly DataContext dataContext;
 
-        public Middleware(ProtectedLocalStorage localStorage)
+        public Middleware(Authentication authentication,
+            ProtectedLocalStorage localStorage,
+            DataContext dataContext)
         {
+            this.authentication = authentication;
             this.localStorage = localStorage;
+            this.dataContext = dataContext;
         }
 
-        public bool Login(string username, string password)
+        public async Task<bool> TryLoginAsync(string username, string password)
         {
-            return false;
+            bool isUserExist = dataContext.Users!
+                    .Any( user => user.Username == username);
+
+            if(!isUserExist)
+            {
+                System.Console.WriteLine("Not Found");
+                return false;
+            }
+
+            User User = dataContext .Users!
+                    .Where(user => user.Username == username)
+                    .FirstOrDefault<User>()!;
+
+            bool isAuthenticated = Cryptograph.HashCheck(password, User!.Password!);
+
+            if(!isAuthenticated)
+            {
+                System.Console.WriteLine("Not Authenticated");
+                return false;
+            }
+            else
+            {
+                ClaimsIdentity user = authentication.CreateIdentity(username);
+                string persistent = JsonSerializer.Serialize<ClaimsIdentity>(user);
+                await localStorage.SetAsync(username, persistent);
+                return true;
+            }
+
+
         }
     }
 }
